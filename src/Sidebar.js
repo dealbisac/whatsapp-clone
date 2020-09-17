7a5c66e6a0c53e87efb218e0f7661dc531c2903e
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-09-17 23:45:55 +0545

    little optimization

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,7 +13,7 @@ function Sidebar() {
     const [rooms, setRooms] = useState([]);
 
     useEffect(() => {
-        db.collection('rooms').onSnapshot((snapshot) =>
+        const unsubscribe = db.collection('rooms').onSnapshot((snapshot) =>
             (
                 setRooms(snapshot.docs.map((doc) => ({
                     id: doc.id,
@@ -22,6 +22,11 @@ function Sidebar() {
                 )
             )
         );
+
+        return () => {
+            unsubscribe();
+        }
+
     }, []);
 
     return (
